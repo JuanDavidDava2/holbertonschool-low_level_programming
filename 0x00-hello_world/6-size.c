47812e9aa4ad0a41a6d21bbca0ef8dc911bb17ac
JuanDavidDava2 <juandm417@gmail.com>
2019-09-18 21:05:06 +0000

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+/**
+*main- Entry point
+*
+* Return: Always 0 (Success)
+*/
+#include <stdio.h>
+int main(void)
+{
+int a = 16;
+printf("Size of variable a : %d\n", sizeof(a));
+printf("Size of int data type : %d\n", sizeof(int));
+printf("Size of char data type : %d\n", sizeof(char));
+printf("Size of float data type : %d\n", sizeof(float));
+printf("Size of double data type : %d\n", sizeof(double));
+return (0);
+}

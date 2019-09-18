36ba100a3f443d7dd24ae5019e47d85686b805d7
JuanDavidDava2 <juandm417@gmail.com>
2019-09-18 21:23:22 +0000

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("Size of int data type : %d\n", (int) sizeof(int));
-	printf("Size of char data type : %d\n", (int) sizeof(char));
-	printf("Size of float data type : %d\n", (int)  sizeof(float));
-	printf("Size of double data type : %d\n", (int) sizeof(double));
+	printf("Size of int data type : %i byte(s)\n", (int) sizeof(int));
+	printf("Size of char data type : %i byte(s)\n", (int) sizeof(char));
+	printf("Size of float data type : %i byte(s)\n", (int)  sizeof(float));
+	printf("Size of double data type : %i byte(s)\n", (int) sizeof(double));
 	return (0);
 }

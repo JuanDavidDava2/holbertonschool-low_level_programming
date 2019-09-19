9313581fb9e6cd22202946f0b8ea4b82ddd897e1
JuanDavidDava2 <juandm417@gmail.com>
2019-09-19 21:09:04 +0000

    fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,11 +9,17 @@
 void checkNumber(int number)
 {
 	if (number > 0)
+	{
 		printf("%d is positive\n", number);
+	}
 	else if (number == 0)
+	{
 		printf("%d is zero\n", number);
+	}
 	else
+	{
 		printf("%d is negative\n", number);
+	}
 }
 /**
 * main - Generate a random number

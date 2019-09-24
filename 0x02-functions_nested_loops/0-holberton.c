e81745d26ab88e1221c8060e614bc5a588797ba6
JuanDavidDava2 <juandm417@gmail.com>
2019-09-24 13:04:33 +0000

    fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -5,15 +5,15 @@
  */
 int main(void)
 {
-	_putchar('H')
-	_putchar('o')
-	_putchar('l')
-	_putchar('b')
-	_putchar('e')
-	_putchar('r')
-	_putchar('t')
-	_putchar('o')
-	_putchar('n')
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }

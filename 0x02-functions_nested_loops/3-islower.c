7f10cd41e6d01f0c835ff7389ad6ea7666be4e6d
JuanDavidDava2 <juandm417@gmail.com>
2019-09-24 15:08:01 +0000

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include"holberton.h"
-/*
-*@int- no argument
-*islower: Returns 1 if c is lowercase
-*
-*/
+/**
+ * _islower - Returns 1 if c is lowercase
+ * @c: an int
+ * Return: 0 blabla
+ */
 int _islower(int c)
 
 {

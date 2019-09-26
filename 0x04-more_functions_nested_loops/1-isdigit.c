4c40cbc7e884d80733a33544d6be32dedb72af06
JuanDavidDava2 <juandm417@gmail.com>
2019-09-26 15:44:30 +0000

    fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,9 @@
 #include"holberton.h"
-/*
-   *
-   *
-   *
-   *
-   */
+/**
+ * _isdigit - print 1 if c is a digit and 0 otherwise
+ * @c: takes int
+ * Return: return 0 or 1
+ */
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')

3c76c6a6874fd38e0f2489c1be60d656af08c2fb
JuanDavidDava2 <juandm417@gmail.com>
2019-10-01 21:15:39 +0000

    fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
-
 /**
- * puts2 - prints every other character of a string, starting with the first character.
+ * puts2 - write other character of a string, starting with the first character
  * @str: takes pointer
  */
 void puts2(char *str)

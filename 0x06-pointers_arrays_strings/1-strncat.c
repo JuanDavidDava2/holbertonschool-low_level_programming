d2aff8f022f3f5efab7b4b87098df5dbc3bb6888
JuanDavidDava2 <juandm417@gmail.com>
2019-10-03 20:32:55 +0000

    fix

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "holberton.hh"
+/**
+  * _strncat - concatenates two strings
+  * @dest: takes  pointer
+  * @src: takes pointer
+  * @n: takes int
+  * Return: none
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		j < n ? dest[i] = src[j] : j;
+	}
+	return (dest);
+}

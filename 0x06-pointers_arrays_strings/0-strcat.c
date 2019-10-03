71a062354f21de7c7c38e1aa7f6941f8cc780cbe
JuanDavidDava2 <juandm417@gmail.com>
2019-10-03 18:50:16 +0000

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,20 @@
-test
+#include "holberton.h"
+
+/**
+ * *_strcat - concatenates two strings.
+ * @dest: takes pointer
+ * @src: takes pointer
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	
+	for (j = 0; (dest[i] = src[j]) != '\0'; i++, j++)
+		;
+	
+	return (dest);
+}

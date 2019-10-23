abe782319e40be60def733838ea0430d44e33199
JuanDavidDava2 <juandm417gmail.com>
2019-10-23 21:17:23 +0000

    op

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include"3-calc.h"
+/**
+ * op_add - returns the sum of a and b
+ * @a: takes int
+ * @b: takes int
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - returns the difference of a and b
+ * @a: takes int
+ * @b: takes int
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns the product of a and b
+ * @a: takes int
+ * @b: takes int
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the result of the division of a by b
+ * @a: takes int
+ * @b: takes int
+ * Return: the division of a by b
+ */
+int op_div(int a, int b)
+{
+	if (a == 0 || b == 0)
+		printf("Error\n");
+	exit(100);
+	return (a / b);
+}
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * @a: takes int
+ * @b: takes int
+ * Return: the division of a by b
+ *
+ */
+int op_mod(int a, int b)
+{
+	if (a == 0 || b == 0)
+		printf("Error\n");
+	exit(100);
+	return (a % b);
+}

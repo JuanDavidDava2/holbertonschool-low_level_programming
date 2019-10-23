9f8ae788705bc836f8e5fa490e0af689f29df131
JuanDavidDava2 <juandm417gmail.com>
2019-10-23 22:44:28 +0000

    get

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include"3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * get_op_func - get the operator
+ * @s: takes sting
+ * Return: pointer
+ */
+int (*get_op_func(char *s))(int, int);
+{
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			break;
+		i++;
+	}
+	return (ops[i].f);
+}

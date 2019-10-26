dc90d9648b57a3762824efea4615c1b0c69ce98e
JuanDavidDava2 <juandm417@gmail.com>
2019-10-26 20:16:43 +0000

    01

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include"variadic_functions.h"
+#include<stdarg.h>
+
+
+
+
+
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list sum;
+	unsigned int s = 0;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+	va_start (sum, n);
+	for (i = 0; i < n; i++)
+		s += va_arg(sum,unsigned int);
+	return (s);
+	va_end (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include"variadic_functions.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdarg.h>
+
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list pri;
+	unsigned int i;
+
+	va_start(pri, n);
+	for (i = 0; i < n; i++)
+	{
+		printf ("%d", va_arg(pri, unsigned int));
+		if (separator != NULL && i != (n - 1))
+				printf("%s", separator);
+	}
+	printf ("\n"); 
+	va_end(pri);
+}

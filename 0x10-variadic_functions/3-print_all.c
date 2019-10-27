a05eee606fe2214b305b232a76d69849f0ef9168
JuanDavidDava2 <juandm417@gmail.com>
2019-10-27 23:29:44 +0000

    3-print.1

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,79 @@
+#include"variadic_functions.h"
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdarg.h>
+/**
+ * prn_char - print char
+ * @ch: takes char..
+ */
+void prn_char(va_list ch)
+{
+	printf("%c", va_arg(ch, int));
+}
+/**
+ * prn_int - print int.
+ * @in: takes int.
+ */
+void prn_int(va_list in)
+{
+	printf("%d", va_arg(in, int));
+}
+/**
+ * prn_float - print float.
+ * @fl: takes float
+ */
+void prn_float(va_list fl)
+{
+	printf("%f", va_arg(fl, double));
+}
+/**
+ * prn_string - print string.
+ * @st: takes string
+ */
+void prn_string(va_list st)
+{
+	char *str = va_arg(st, char *);
+
+	if (str == '\0')
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+/**
+ * print_all - print char, int, float or string.
+ * @format: take a string
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	char *sep = "";
+	int i = 0, j;
+
+	op_t ops[] = {
+		{"c", prn_char},
+		{"i", prn_int},
+		{"f", prn_float},
+		{"s", prn_string},
+		{NULL, NULL}
+	};
+	va_start(list, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (ops[j].op)
+		{
+			if (ops[j].op[0] == format[i])
+			{
+				printf("%s", sep);
+				ops[j].f(list);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}

cecd12e53de6bb398232fe576e22b9c544ebd3b6
JuanDavidDava2 <juandm417@gmail.com>
2019-11-01 21:13:57 +0000

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,16 +6,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count;
+	size_t count = 0;
 
+	if (h == NULL)
+		return (0);
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
-return (count);
+	return (count);
 }

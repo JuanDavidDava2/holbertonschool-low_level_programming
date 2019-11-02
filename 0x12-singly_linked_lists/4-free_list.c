ca79d4a952a1d719a70c4d8792f541832285acbf
JuanDavidDava2 <juandm417@gmail.com>
2019-11-02 03:18:02 +0000

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,15 +2,14 @@
 /**
  * free_list - free memory of list
  * @head: list
- * Return: list
  */
 
 void free_list(list_t *head)
 {
 	if (head)
 	{
-	free(head->next);
-	free(head->str);
-	free(head);
+		free(head->next);
+		free(head->str);
+		free(head);
 	}
 }

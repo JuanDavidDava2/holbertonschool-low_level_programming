4d966c0d60c587dde81dc6bbfffbf2d4f806a235
JuanDavidDava2 <juandm417@gmail.com>
2019-11-05 15:39:28 +0000

    chance name

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_nodeint - add a new node.
+ * @head: list
+ * @n: new element
+ * Return: address of elements of list
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+
+	return(*head);
+}
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,16 @@
+#include"lists.h"
+/**
+  *
+  *
+  *
+  *
+  *
+  */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t new;
+
+	new = malloc(sizeof(listint_t));
+
+
+}

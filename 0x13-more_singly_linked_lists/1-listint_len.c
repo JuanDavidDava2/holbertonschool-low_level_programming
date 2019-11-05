6c77b66b8390dc16b6f1e1bb28075ff22b046a17
JuanDavidDava2 <juandm417@gmail.com>
2019-11-05 15:46:53 +0000

    correct betty

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 /**
  * listint_len - return the number of elements in a linked listint_t list.
- * @h: list.	
+ * @h: list.
  * Return: number of elements of list
  */
-
 size_t listint_len(const listint_t *h)
 {
 	unsigned int i = 0;

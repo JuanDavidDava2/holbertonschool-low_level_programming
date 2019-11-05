319ee2b104ae4b5bb4a1148e273e8f46facbc846
JuanDavidDava2 <juandm417@gmail.com>
2019-11-05 05:47:53 +0000

    adv

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+	int p, s, t;
+
+	for (s = 0; s <= 9; s++)
+	{
+		for (p = 0; p <= 9; p++)
+	{
+			for (t = 0; t <= 9; t++)
+
+				if (s != p && p > s && t > s && p != t && s != t && t > s && t > p)
+				{
+					putchar (s + '0');
+					putchar (p + '0');
+					putchar (t + '0');
+					if (s != 7 || p != 8 || t != 9)
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}

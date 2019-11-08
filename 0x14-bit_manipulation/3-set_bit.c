02bd68b66670f50defa37f89ac5905cf577eb0ff
JuanDavidDava2 <juandm417@gmail.com>
2019-11-08 04:50:16 +0000

    fix betty

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * set_bit - sets the value of a bit to 1 
+ * set_bit - sets the value of a bit to 1
  * @n: take int
  * @index: take int
  * Return: the number
@@ -9,7 +9,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(n) * 8)
 		return (-1);
-	else
 	{
 		*n = *n + (1 << index);
 		return (1);

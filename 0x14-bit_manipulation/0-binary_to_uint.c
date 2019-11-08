27a3b60bfc0f7db28088547db0fd44c08a4a58ea
JuanDavidDava2 <juandm417@gmail.com>
2019-11-08 00:54:40 +0000

    convert to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - convert binary to decimal
+ * @b: take binary
+ * Return: number decimal.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int s = 0;
+	unsigned int num = 0;
+
+	if (b == NULL)
+		return (-1);
+	while (b[s] != '\0')
+	{
+		if (b[s] == '1' || b[s] == '0')
+			num = num * 2 +  (b[s] - '0');
+		else
+			return (0);
+		s++;
+	}
+	return (num);
+}

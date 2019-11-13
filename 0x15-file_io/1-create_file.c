d9547adb2602d5e5d675334017581edb629f9961
JuanDavidDava2 <juandm417@gmail.com>
2019-11-13 02:14:23 +0000

    new funtion

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,21 @@
 #include "holberton.h"
 /**
+ * _strlen - length string
+ * @s: string
+ * Return: int string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	if (!s)
+		return (0);
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
  * create_file - creates a file
  * @filename: name of file
  * @text_content: content to put in file
@@ -7,14 +23,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, len;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (0);
-	if (!text_content)
+	len = _strlen(text_content);
+	if (write(fd, text_content, len) != len)
 		return (-1);
-	return (0);
+	close(fd);
+	return (1);
+
 }

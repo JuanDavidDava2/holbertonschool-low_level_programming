92ecd84ea2658196042402128c2d772a3538318f
JuanDavidDava2 <juandm417@gmail.com>
2019-11-13 22:41:28 +0000

    fix open

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,12 +11,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content)
-	{
-	for (len = 0; text_content[len] < '\0'; len++)
+	if (!text_content)
+		text_content = "";
+	for (len = 0; text_content[len] != '\0'; len++)
 		;
 	fd = write(fd, text_content, len);
 	if (fd == -1)                     

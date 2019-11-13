abdaacef60323082032176d12be98ddaf4c64a09
JuanDavidDava2 <juandm417@gmail.com>
2019-11-13 02:52:47 +0000

    idk2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,13 +23,14 @@ int _strlen(char *s)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = _strlen(text_content);
+	int fd, len;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (0);
+	len = _strlen(text_content);
 	if (write(fd, text_content, len) != len)
 		return (-1);
 	close(fd);

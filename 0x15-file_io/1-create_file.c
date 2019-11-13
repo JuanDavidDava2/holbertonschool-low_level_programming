7f8b709c6d7e5ed92a73cd6e8d9bef7f65f02ce2
JuanDavidDava2 <juandm417@gmail.com>
2019-11-13 02:49:57 +0000

    idk

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@ int _strlen(char *s)
 {
 	int i;
 
-	if (!s)
+	if (s)
 		return (0);
 
 	for (i = 0; s[i]; i++)
@@ -23,17 +23,15 @@ int _strlen(char *s)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len;
+	int fd, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (0);
-	len = _strlen(text_content);
 	if (write(fd, text_content, len) != len)
 		return (-1);
 	close(fd);
 	return (1);
-
 }

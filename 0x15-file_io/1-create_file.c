203cec40104dd96556e237b2bec6e3f0e0daaa0b
JuanDavidDava2 <juandm417@gmail.com>
2019-11-13 22:28:35 +0000

    idk again

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,10 +13,15 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
+	if (text_content)
+	{
 	for (len = 0; text_content[len] < '\0'; len++)
 		;
 	fd = write(fd, text_content, len);
+	if (fd == -1)                     
+		return (-1);
+	}
 	close(fd);
 	return (1);
 

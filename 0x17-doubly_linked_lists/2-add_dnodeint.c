8b907065cd03fa13518760c384e1fc9f72833c76
JuanDavidDava2 <juandm417@gmail.com>
2019-12-16 04:20:05 +0000

    fix add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,8 +13,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->next = *head;
-	new->prev = NULL;
-	*head = new;
+	if (*head == NULL)            
+	{
+		*head = new;                                         
+		new->prev = NULL;
+		new->next= NULL;
+	}
+	else
+	{
+		new->next = *head;
+		new->prev = NULL;
+		*head = new;
+	}
 	return (new);
 }

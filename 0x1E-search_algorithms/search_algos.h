8a724c8d9def56c4029c27413ffe16aa26da8f5a
JuanDavidDava2 <juandm417@gmail.com>
2020-04-19 01:50:16 +0000

    add funtions

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,8 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t size);
 #endif
